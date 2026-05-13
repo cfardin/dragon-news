625dc8118011ab6bb2d7013d53f4f8969718394e
cfardin <cfardin51@gmail.com>
2026-05-13 16:04:36 +0600

    added header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+"use client";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import { format, compareAsc } from "date-fns";
 
 const Header = () => {
-    return (
+
+//   console.log(now);
+
+  return (
+    <div className=" text-center py-8 space-y-5">
+
+      {/* logo */}
+      <div>
+        <Image
+          src={logo}
+          width={300}
+          height={200}
+          className="mx-auto"
+          alt="logo-the-dragon-news"
+        ></Image>
+      </div>
+
+      {/* header text */}
+      <div className="space-y-2.5">
+        <p>Journalism Without Fear or Favour</p>
         <div>
-            <h2>this is header</h2>
+          <h3>{format(new Date(), "EEEE, MMM dd, yyyy")}</h3>
         </div>
-    );
+      </div>
+
+      <div>
+
+      </div>
+    </div>
+  );
 };
 
 export default Header;
